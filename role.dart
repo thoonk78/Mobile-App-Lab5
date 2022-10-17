f7ddb86a3141dfcdd9f17282028b0ef364b3a163
mise11 <115748357+mise11@users.noreply.github.com>
2022-10-16 21:01:50 -0500

    Update role.dart

--- role.dart
+++ role.dart
@@ -4,4 +4,15 @@ class Role {
   // ignore: prefer_typing_uninitialized_variables
   var year;
   Role({required this.answer, required this.clue, this.year});
+  String getAnswer() {
+    return answer;
+  }
+
+  String getclue() {
+    return clue;
+  }
+
+  String getDate() {
+    return year;
+  }
 }
